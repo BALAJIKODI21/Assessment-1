1f71b0a3438403892977c9e218928501cda8f791
Balaji K <balaji.kr@prodapt.com>
2022-07-19 09:38:26 +0530

    Source code

---
+++ SchoolRecords.java
@@ -0,0 +1,16 @@
+package prodapt25java;
+
+public class SchoolRecords {
+	int schoolId;
+	String schoolName;
+	String schoolAddress;
+	void addRecords(int schoolId, String schoolName, String schoolAddress) {
+	System.out.println(schoolId + " " + " " + schoolName + " " + schoolAddress);
+	}
+	void displayRecords()
+	{
+		System.out.println("Records of Schools in Tamilnadu");
+	
+	}
+
+}
