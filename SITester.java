080644a6a1663de7b36a8a0ba1b782db073cc958
Balaji K <balaji.kr@prodapt.com>
2022-07-19 09:48:12 +0530

    Tester of SI

---
+++ SITester.java
@@ -0,0 +1,14 @@
+package prodapt25java;
+
+public class SITester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SI.principle = 4500;
+		SI.rate = 15;
+		SI.time = 1;
+		SI.simpleInterest();
+
+	}
+
+}
