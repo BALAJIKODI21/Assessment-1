fa65e470636364b9656bdf8e2eb17b3c60dbe0b8
Balaji K <balaji.kr@prodapt.com>
2022-07-19 09:43:35 +0530

    Tester of SchoolRecords

---
+++ SchoolRecordsTester.java
@@ -0,0 +1,16 @@
+package prodapt25java;
+
+public class SchoolRecordsTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SchoolRecords ob1 = new SchoolRecords();
+		ob1.displayRecords();
+		ob1.addRecords(1, "Sri Ambika Vidhyalaya Matriculation", "Cuddalore" );
+		ob1.addRecords(2, "Sri Chaitanya School", "Chennai" );
+		ob1.addRecords(3, "Bhanodaya International School", "Tirutani" );
+		ob1.addRecords(4, "National School", "Namakkal");
+
+	}
+
+}
